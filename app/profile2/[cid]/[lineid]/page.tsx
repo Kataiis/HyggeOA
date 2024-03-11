89149488fb4935e0d78fcfc0747a76fc7ef11366
kataiis <mj.supansa@gmail.com>
2024-03-11 15:29:35 +0700

    update checkprofile 2

--- app/profile2/[cid]/[lineid]/page.tsx
+++ app/profile2/[cid]/[lineid]/page.tsx
@@ -11,6 +11,8 @@ import QRCode from "react-qr-code";
 
 import logo from "@/public/hg.png"
 import axios from "axios";
+import { el } from "date-fns/locale";
+import { CirclesWithBar } from "react-loader-spinner";
 
 const hyggeOAliff: any = process.env.HyggeOAliff;
 
@@ -22,17 +24,42 @@ const ProfilePage = ({ params }: { params: { cid: string, lineid: string } }) =>
     const [patient, setPatient] = useState<any>([]);
 
     const [lineId, setLineId] = useState("");
-    const [loading, setloading] = useState(false);
+    const [loading, setloading] = useState(true);
     const [image, setimage] = useState("");
 
     const imgPath = 'https://www.virtualhos.net/api4000/apihygge/getImageProfile/' + params.cid;
 
-    useEffect(() => {
 
+
+
+    const check = async () => {
+        setloading(true);
+        console.log("dataSend", params.cid)
+        console.log("dataSend", params.lineid)
+
+        const checkdata = await axios.post(`${pathUrl}/health/hyggelineservice/checkCitizen`, { cid: params.cid, lineid: params.lineid })
+        console.log("checkdata : ", checkdata.data)
+        if (checkdata.data.ok) {
+            console.log("length", checkdata.data.message)
+            if (checkdata.data.message > 0) {
+                setloading(false)
+            }
+            else {
+                router.replace("/login")
+            }
+        }
+        else {
+
+            throw new Error(checkdata.data.error);
+        }
+
+    };
+
+
+    useEffect(() => {
+        check();
         const getPatient = async () => {
             const res: any = await axios.post(`${pathUrl}/health/hygge_citizen/bycid`, { cid: params.cid }).then((v: any) => setPatient(v.data.message[0]));
-
-
         }
         getPatient();
 
@@ -50,7 +77,7 @@ const ProfilePage = ({ params }: { params: { cid: string, lineid: string } }) =>
     return (
 
         <div>
-          
+
             <div className='bg-[#F15D4F] flex justify-center p-3 '>
                 <Image
                     priority
@@ -86,55 +113,75 @@ const ProfilePage = ({ params }: { params: { cid: string, lineid: string } }) =>
                     height={100} /> */}
 
             {/* </div> */}
-
-
-            <div className="grid grid-cols-2 justify-items-center m-5 ">
-                <div className=" p-5 ">
-                    <Avatar src={imgPath} sx={{ width: 140, height: 140 }} />
-                </div>
-
-                <div className=" p-5">
-                    <QRCode
-                        size={145}
-                        // style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                        value={`${params?.cid}`}
-                        viewBox={`0 0 256 256`}
+            {loading && (
+                <div className="flex flex-row justify-center items-center w-full mt-10">
+                    <CirclesWithBar
+                        height="100"
+                        width="100"
+                        color="#00AE91"
+                        outerCircleColor="#00AE91"
+                        innerCircleColor="#00AE91"
+                        barColor="#00AE91"
+                        ariaLabel="circles-with-bar-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
                     />
+
                 </div>
-            </div>
+            )}
 
-            <div className='m-5 text-2xl'>
-                <p className='text-center text-[#2C97A3]'>
-                    {patient?.pname} {patient?.fname} {patient?.lname}
-                </p>
-            </div>
-            <div className='bg-[#39CC88]  mt-3'>
-                <p className='text-center text-xl text-[#ffffff] align-middle p-2'>สิทธิ :  {patient?.pttype_name} </p>
+            {!loading && (
 
-            </div>
-            <div className='grid justify-items-center m-8  gap-6'>
+                <div>
+                    <div className="grid grid-cols-2 justify-items-center m-5 ">
+                        <div className=" p-5 ">
+                            <Avatar src={imgPath} sx={{ width: 140, height: 140 }} />
+                        </div>
 
-                <Button className="bg-[#2C97A3] text-[#ffffff] text-xl h-14 w-full rounded-xl shadow-md shadow-gray-500/100 "
-                    type="button"
-                    onClick={() => router.replace("../" + params.cid + "/" + params.lineid + "/healthbook")}
-                >สมุดสุขภาพ
-                </Button>
-                <div className="-mt-3"> สมุดบันทึกข้อมูล <span className="text-lg italic ">ด้วยตัวคุณเอง</span></div>
+                        <div className=" p-5">
+                            <QRCode
+                                size={145}
+                                // style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                                value={`${params?.cid}`}
+                                viewBox={`0 0 256 256`}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='m-5 text-2xl'>
+                        <p className='text-center text-[#2C97A3]'>
+                            {patient?.pname} {patient?.fname} {patient?.lname}
+                        </p>
+                    </div>
+                    <div className='bg-[#39CC88]  mt-3'>
+                        <p className='text-center text-xl text-[#ffffff] align-middle p-2'>สิทธิ :  {patient?.pttype_name} </p>
+
+                    </div>
+                    <div className='grid justify-items-center m-8  gap-6'>
+
+                        <Button className="bg-[#2C97A3] text-[#ffffff] text-xl h-14 w-full rounded-xl shadow-md shadow-gray-500/100 "
+                            type="button"
+                            onClick={() => router.replace("../" + params.cid + "/" + params.lineid + "/healthbook")}
+                        >สมุดสุขภาพ
+                        </Button>
+                        <div className="-mt-3"> สมุดบันทึกข้อมูล <span className="text-lg italic ">ด้วยตัวคุณเอง</span></div>
 
 
 
-                <Button className="bg-[#49DABD] text-[#ffffff] text-xl h-14 w-full rounded-xl shadow-md shadow-gray-500/100"
-                    type="button"
-                    // onClick={() => updatedata()}
-                    onClick={() => router.replace("../" + params.cid + "/" + params.lineid + "/patient")}
+                        <Button className="bg-[#49DABD] text-[#ffffff] text-xl h-14 w-full rounded-xl shadow-md shadow-gray-500/100"
+                            type="button"
+                            // onClick={() => updatedata()}
+                            onClick={() => router.replace("../" + params.cid + "/" + params.lineid + "/patient")}
 
-                >สมุดโรงพยาบาล</Button>
+                        >สมุดโรงพยาบาล</Button>
 
-                <div className="-mt-3">ข้อมูลสุขภาพ <span className="text-lg italic ">จากโรงพยาบาล </span></div>
+                        <div className="-mt-3">ข้อมูลสุขภาพ <span className="text-lg italic ">จากโรงพยาบาล </span></div>
 
 
-            </div>
-
+                    </div>
+                </div>
+            )}
 
         </div>
     )
