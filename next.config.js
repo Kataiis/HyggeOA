49561315dad65e79e1088bfc35213cb9ef709334
MMondayS <nutgamol.mah@gmail.com>
2024-03-14 11:06:02 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -6,11 +6,11 @@ const nextConfig = {
     },
     env: {
         pathUrl: `https://hyggemedicalservice.com/phpapi/api`,
-        // HyggeOAliff: '2003391401-jDvm8MlZ', 
+        HyggeOAliff: '2003391401-jDvm8MlZ', 
         APIKey: 'https://apimobile.hyggecode.com',
         
         // test //
-        HyggeOAliff: '2002893867-MzGeD6nL',
+        // HyggeOAliff: '2002893867-MzGeD6nL',
   
     }
 
